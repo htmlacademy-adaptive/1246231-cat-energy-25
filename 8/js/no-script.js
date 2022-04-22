c92968288a2863f5996312e940b5909040550055
Keks <keks@htmlacademy.ru>
2022-04-22 21:09:47 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/no-script.js
@@ -0,0 +1,9 @@
+const headerNoJs = document.querySelector('.header--no-js');
+const hamburgerNoJs = document.querySelector('.header__hamburger--no-js');
+const navigationListNoJs = document.querySelector('.site-navigation--no-js');
+
+headerNoJs.classList.remove('header--no-js');
+
+hamburgerNoJs.classList.remove('header__hamburger--no-js');
+
+navigationListNoJs.classList.remove('site-navigation--no-js');
