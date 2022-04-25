9b9b362d28e186dc388c0795db07eeed79c22f5c
Vladimir Polozov <65243518+RedShucov@users.noreply.github.com>
2022-04-25 19:35:03 +0300

    Отрекдактированны стили и разметка слайдера

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,1 +1,6 @@
-// переделать
+const slider = document.querySelector('.slider');
+const sliderRange = document.querySelector('.slider__range-buton');
+
+function changeSliderValue() {
+  slider.style.setProperty("--slider-width", sliderRange.value + "%");
+}
